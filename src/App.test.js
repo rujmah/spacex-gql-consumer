48613bad3fae9c57f6d457ac69e8d18c0c2a0d8b
Robin Mayfield <robin@degu.io>
2021-07-15 15:17:52 +0100

    fixed test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/SpaceX/i);
+  const linkElement = screen.getByText(/Test GQL/i);
   expect(linkElement).toBeInTheDocument();
 });
